04a0649eafb2bfacc911a146684a5a7fe5cdeb9e
Sakkie Thipane <123340673+Mphare1@users.noreply.github.com>
2023-08-31 16:50:20 +0200

    Update binary_tree_print.c

--- binary_tree_print.c
+++ binary_tree_print.c
@@ -61,36 +61,29 @@ static size_t _height(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_print - it Prints binary tree
- * @tree: Pointer to the root node
+ * binary_tree_print - Prints a binary tree
+ *
+ * @tree: Pointer to the root node of the tree to print
  */
 void binary_tree_print(const binary_tree_t *tree)
 {
 	char **s;
 	size_t height, i, j;
 
-  
 	if (!tree)
 		return;
-  
 	height = _height(tree);
 	s = malloc(sizeof(*s) * (height + 1));
-  
 	if (!s)
 		return;
-  
 	for (i = 0; i < height + 1; i++)
 	{
 		s[i] = malloc(sizeof(**s) * 255);
-    
 		if (!s[i])
 			return;
-    
 		memset(s[i], 32, 255);
 	}
-  
 	print_t(tree, 0, 0, s);
-  
 	for (i = 0; i < height + 1; i++)
 	{
 		for (j = 254; j > 1; --j)
@@ -102,6 +95,5 @@ void binary_tree_print(const binary_tree_t *tree)
 		printf("%s\n", s[i]);
 		free(s[i]);
 	}
-  
 	free(s);
 }
