44bd5d3e8e1ababa9c6c856d18a4fa29b29fb445
Lunga-28 <124337357+Lunga-28@users.noreply.github.com>
2023-08-31 10:11:40 +0200

    Create 10-binary_tree_depth.c

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - ...
+ * @tree: ...
+ * Return: If return 0, else return the depth.
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+    if (tree && tree->parent)
+    {
+        return 1 + binary_tree_depth(tree->parent);
+    }
+    else
+    {
+        return 0;
+    }
+}
