b4177974f37e388a94bc6166d4cb55cc038220e7
Lunga-28 <124337357+Lunga-28@users.noreply.github.com>
2023-08-31 19:57:49 +0200

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,43 +1,51 @@
-include "binary_trees.h"
+#include "binary_trees.h"
 
-unsigned char is_leaf(const binary_tree_t *node);
-size_t depth(const binary_tree_t *tree);
-const binary_tree_t *get_leaf(const binary_tree_t *tree);
-int is_perfect_recursive(const binary_tree_t *tree,
-		size_t leaf_depth, size_t level);
-int binary_tree_is_perfect(const binary_tree_t *tree);
-
-unsigned char is_leaf(const binary_tree_t *node)
-{
-	return ((node->left == NULL && node->right == NULL) ? 1 : 0);
-}
-
-size_t depth(const binary_tree_t *tree)
-{
-	return (tree->parent != NULL ? 1 + depth(tree->parent) : 0);
-}
-
-const binary_tree_t *get_leaf(const binary_tree_t *tree)
-{
-	if (is_leaf(tree) == 1)
-		return (tree);
-	return (tree->left ? get_leaf(tree->left) : get_leaf(tree->right));
-}
-
-int is_perfect_recursive(const binary_tree_t *tree,
-		size_t leaf_depth, size_t level)
-{
-	if (is_leaf(tree))
-		return (level == leaf_depth ? 1 : 0);
-	if (tree->left == NULL || tree->right == NULL)
-		return (0);
-	return (is_perfect_recursive(tree->left, leaf_depth, level + 1) &&
-		is_perfect_recursive(tree->right, leaf_depth, level + 1));
-}
-
-int binary_tree_is_perfect(const binary_tree_t *tree)
+/**
+ * binary_tree_height - Calculate the height of a binary tree.
+ * @tree: A pointer to the root node of the tree.
+ * Return: The height of the tree.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	return (is_perfect_recursive(tree, depth(get_leaf(tree)), 0));
+
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
+
+	return (1 + (left_height > right_height ? left_height : right_height));
+}
+
+/**
+ * binary_tree_is_perfect - Check if a binary tree is perfect.
+ * @tree: A pointer to the root node of the tree.
+ * Return: 1 if the tree is perfect, 0 otherwise.
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	size_t height = binary_tree_height(tree);
+	int result = 1;
+
+	if (tree == NULL)
+		return (0);
+
+	/* Helper function to perform level-order traversal */
+	void level_order(const binary_tree_t *node, size_t current_level)
+	{
+		if (node == NULL)
+			return;
+
+		if (current_level == height - 1 && (node->left || node->right))
+		{
+			result = 0;
+			return;
+		}
+
+		level_order(node->left, current_level + 1);
+		level_order(node->right, current_level + 1);
+	}
+
+	level_order(tree, 0);
+
+	return result;
 }
