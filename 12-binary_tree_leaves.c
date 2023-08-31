e8c3e3ee6dc0e3b9685136935737e413525fab80
Lunga-28 <124337357+Lunga-28@users.noreply.github.com>
2023-08-31 10:13:26 +0200

    Create 12-binary_tree_leaves.c

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - ...
+ * @tree: ...
+ * Return: ...
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t lves = 0;
+	if (tree)
+	{
+		lves += (!tree->left && !tree->right) ? 1 : 0;
+		lves += binary_tree_leaves(tree->left);
+		lves += binary_tree_leaves(tree->right);
+	}
+	return (lves);
+}
