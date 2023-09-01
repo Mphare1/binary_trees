7ad4fed11fdd1f2ce43850ff662737fb0aa12907
Lunga-28 <124337357+Lunga-28@users.noreply.github.com>
2023-09-01 21:40:39 +0200

    Create 100-long_binary_trees_ancestor.c

---
+++ 100-long_binary_trees_ancestor.c
@@ -0,0 +1,49 @@
+#include "binary_trees.h"
+
+size_t depth(const binary_tree_t *tree);
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
+{
+	size_t first_depth, second_depth;
+	if (first == NULL || second == NULL)
+		return (NULL);
+  
+	if (first == second)
+		return ((binary_tree_t *)first);
+  
+	if (first->parent == second->parent)
+		return ((binary_tree_t *)first->parent);
+  
+	if (first == second->parent)
+		return ((binary_tree_t *)first);
+  
+	if (first->parent == second)
+		return ((binary_tree_t *)second);
+
+	for (first_depth = depth(first); first_depth > 1; first_depth--)
+		first = first->parent;
+  
+	for (second_depth = depth(second); second_depth > 1; second_depth--)
+		second = second->parent;
+
+	if (first == second)
+		return ((binary_tree_t *)first);
+  
+	if (first->parent == second->parent)
+		return ((binary_tree_t *)first->parent);
+  
+	if (first == second->parent)
+		return ((binary_tree_t *)first);
+  
+	if (first->parent == second)
+		return ((binary_tree_t *)second);
+    
+	else
+		return (NULL);
+}
+
+size_t depth(const binary_tree_t *tree)
+{
+	return ((tree->parent != NULL) ? 1 + depth(tree->parent) : 0);
+}
