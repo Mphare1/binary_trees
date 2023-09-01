5ef58a72d1a56d4fd5b4514e563a65d2d724da60
Lunga-28 <124337357+Lunga-28@users.noreply.github.com>
2023-09-01 21:43:23 +0200

    Create 100-short_binary_trees_ancestor.c

---
+++ 100-short_binary_trees_ancestor.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
+{
+	binary_tree_t *mom, *pops;
+
+	if (!first || !second)
+		return (NULL);
+  
+	if (first == second)
+		return ((binary_tree_t *)first);
+
+	mom = first->parent, pops = second->parent;
+	if (first == pops || !mom || (!mom->parent && pop))
+		return (binary_trees_ancestor(first, pops));
+    
+	else if (mom == second || !pops || (!pops->parent && mom))
+		return (binary_trees_ancestor(mom, second));
+  
+	return (binary_trees_ancestor(mom, pops));
+}
